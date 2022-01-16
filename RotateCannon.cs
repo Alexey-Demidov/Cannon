dfcd9a40e1e4414da1e93c5ebf295f5200e9a7b1
Alexey-Demidov <93328889+Alexey-Demidov@users.noreply.github.com>
2022-01-16 17:46:47 +0700

    Update RotateCannon.cs

--- RotateCannon.cs
+++ RotateCannon.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class RotateCannon : CannonComponents
 {
-    private float _rotationX;   // поворот вверх-вниз 
-    private float _rotationY;   // поворот влево-вправо
+    private float _rotationX;   
+    private float _rotationY;   
 
-    [Header("Ускорение вращения. Настройка из редактора.")]
-    [Range(1, 10)] [SerializeField] private float _acceleration = 1;  // ускорение поворота
+    [Header("РЈСЃРєРѕСЂРµРЅРёРµ РІСЂР°С‰РµРЅРёСЏ. РЈСЃС‚Р°РЅРѕРІРєР° РёР· РёРЅСЃРїРµРєС‚РѕСЂР°.")]
+    [Range(1, 10)] [SerializeField] private float _acceleration = 1;  
 
-    [Header("Ограничения вращения по углам. Настройка из редактора.")]
+    [Header("РњР°РєСЃРёРјР°Р»СЊРЅС‹Рµ Рё РјРёРЅРёРјР°Р»СЊРЅС‹Рµ СѓРіР»С‹ РїРѕРІРѕСЂРѕС‚Р°. РЈСЃС‚Р°РЅРѕРІРєР° РёР· РёРЅСЃРїРµРєС‚РѕСЂР°.")]
     [Range(10, 40)] [SerializeField] private float _maxRotationX = 25;
     [Range(10, 40)] [SerializeField] private float _minRotationX = -25;
     [Range(10, 40)] [SerializeField] private float _maxRotationY = 25;
@@ -17,7 +17,7 @@ public class RotateCannon : CannonComponents
     public override void Execute()
     {
         _rotationX += Input.GetAxis("Mouse X") *    _acceleration;
-        _rotationY += Input.GetAxis("Mouse Y") * (- _acceleration);  // отрицательным значением _acceleration избавляемся от инверсии
+        _rotationY += Input.GetAxis("Mouse Y") * (- _acceleration);  
 
         ValidateAngles();
 
@@ -26,17 +26,15 @@ public class RotateCannon : CannonComponents
     public override void Disable() => this.enabled = false;
     public override void Enable() => this.enabled = true;
 
-    private void ValidateAngles()   // не дает выйти за максимальное значение поворота
+    private void ValidateAngles()   
     {
         if (_rotationX > _maxRotationX)
             _rotationX = _maxRotationX;
-
         else if (_rotationX < _minRotationX)
             _rotationX = _minRotationX;
 
         if (_rotationY > _maxRotationY)
             _rotationY = _maxRotationY;
-
         else if (_rotationY < _minRotationY)
             _rotationY = _minRotationY;
     }
