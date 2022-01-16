089ebec64de162939b5132fb1043a5cad36b93f6
Alexey-Demidov <93328889+Alexey-Demidov@users.noreply.github.com>
2022-01-16 18:09:49 +0700

    Update and rename ShotPrefab.cs to ProjectilePrefab.cs

--- ProjectilePrefab.cs
+++ ProjectilePrefab.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
 
-// Управление поведением префаба пушечного ядра
-
 [RequireComponent(typeof(Rigidbody))]
 public class ShotPrefab : MonoBehaviour
 {
-    // сохраняем твердое тело объекта для возмоности расширения игровой логики
     private Rigidbody _rigidbody;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         Destroy(gameObject, 3f);
-    }
-
-    
+    }    
 }
